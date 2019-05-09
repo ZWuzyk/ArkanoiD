9780605a78da14a6803229fe3334c1eaff070713
ZWuzyk <zuwuzyk@gmail.com>
2019-05-09 12:35:08 +0200

    Clean up of old and experimental stuff + added Ball script

---
+++ ArkanoiD/Assets/Scenes/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    [SerializeField] Paddle paddle1;
+
+    Vector2 paddleToBallVector;
+
+	// Use this for initialization
+	private void Start () {
+
+        paddleToBallVector = transform.position - paddle1.transform.position;
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.simulated = false;
+    } 
+	
+	// Update is called once per frame
+	private void Update () {
+
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+	}
+}
